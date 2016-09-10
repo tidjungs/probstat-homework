31cb7a66b6ce6e3bb49da203116dd7509d4a73e0
tidjungs <waewprach.s@ku.th>
2016-09-11 00:52:36 +0700

    fix getPx and add fact.js

---
+++ fact.js
@@ -0,0 +1,2 @@
+export default const fact = (n) => n == 1 || n == 0 ? 1 : n * fact(n-1)
+
--- getPxLog.js
+++ getPxLog.js
@@ -1,4 +1,7 @@
+let cdf = 0;
 for(let x=1; x<=9; x++) {
 	let p = 10*Math.log10((x+1)/x);
-	console.log(`P(${x}): ` + p + '\n');
+	console.log(`P(${x}): ` + p.toFixed(2));
+	cdf += p;
+	console.log(`f(${x}): ` + cdf.toFixed(2) + '\n');
 }
