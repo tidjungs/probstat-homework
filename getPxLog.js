68834f90a18ba0dab47f01a688ad85c339f5eb42
tidjungs <waewprach.s@ku.th>
2016-09-13 14:04:24 +0700

    change pxlog

--- getPxLog.js
+++ getPxLog.js
@@ -1,7 +1,7 @@
 let cdf = 0;
 for(let x=1; x<=9; x++) {
-	let p = 10*Math.log10((x+1)/x);
-	console.log(`P(${x}): ` + p.toFixed(2));
+	let p = Math.log10((x+1)/x);
+	console.log(`P(${x}): ` + p.toFixed(3));
 	cdf += p;
-	console.log(`f(${x}): ` + cdf.toFixed(2) + '\n');
+	console.log(`f(${x}): ` + cdf.toFixed(3) + '\n');
 }
--- poisson.js
+++ poisson.js
@@ -2,11 +2,12 @@ import fact from './fact'
 
 let sum = 0;
 let param = 20;
-for(let x=0;x<=30;x++) {
+for(let x=0;x<=10;x++) {
 	let p = Math.pow(Math.E, -param) * Math.pow(param, x) / fact(x)
 	console.log(`P(${x}): ` + p)
 	sum += p
 }
 
 console.log(`SUM = ` + sum)
+console.log(fact(20))
 // console.log(`ANS = ` + (1-sum))
